a6ec8b7a5bbb1d747627a49996e80ebdc7624c8b
PHOENUX\Brandon <bdstevenson4225@aol.com>
2020-02-06 21:17:00 -0500

    Updates

---
+++ precache-manifest.d10f7cfec91f73a3becb4b51abb545d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "215e4f2e93a02f1bdd15447ce9b50a64",
+    "url": "/Kanji-Helper/index.html"
+  },
+  {
+    "revision": "2a72369d763c68cc48ef",
+    "url": "/Kanji-Helper/static/css/main.723df43d.chunk.css"
+  },
+  {
+    "revision": "951a5fdc3c91abcd3527",
+    "url": "/Kanji-Helper/static/js/2.237d962d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Kanji-Helper/static/js/2.237d962d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a72369d763c68cc48ef",
+    "url": "/Kanji-Helper/static/js/main.de383b65.chunk.js"
+  },
+  {
+    "revision": "09d641a019fe5ca05072",
+    "url": "/Kanji-Helper/static/js/runtime-main.1a8840c0.js"
+  }
+]);
